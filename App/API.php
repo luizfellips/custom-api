bcbb905337f13344ef0fb417ae73fb3b32c7c04b
Luiz Fellipe <luizfellipebr03@gmail.com>
2023-04-18 14:32:20 -0300

    Unnecessary and increasingly complex version of api class xd

--- App/API.php
+++ App/API.php
@@ -2,33 +2,31 @@
 
 namespace App;
 
-class API{
+class API
+{
     private $url;
-    private $service;
-    private $method;
-    private $id = null;
 
     public function __construct($url)
     {
         $this->url = explode('/', $url);
-        if($this->url[0] === 'api'){
-            array_shift($this->url);
-            $this->service = 'App\Services\\' . ucfirst($this->url[0] . 'Service');
-            $this->id = $this->url[1];
-            $this->method = strtolower($_SERVER['REQUEST_METHOD']);
-        }
     }
 
-    public function execute(){
-        if(!$this->service || !$this->method){
+    public function execute()
+    {
+        if (!strtolower($_SERVER['REQUEST_METHOD']) || !('App\Services\\' . ucfirst($this->url[1] . 'Service'))) {
             http_response_code(404);
             echo json_encode(array('status' => 'error', 'data' => 'Endpoint not found'), JSON_UNESCAPED_UNICODE);
             exit;
         }
         try {
-            $response = call_user_func_array(array(new $this->service, $this->method), array($this->id));
             http_response_code(200);
-            echo json_encode(array('status' => 'success', 'data' => $response));
+            echo json_encode(array('status' => 'success', 'data' => call_user_func_array(
+                array(
+                    new ('App\Services\\' . ucfirst($this->url[1] . 'Service')),
+                    strtolower($_SERVER['REQUEST_METHOD'])
+                ),
+                array($this->url[2])
+            )));
             exit;
         } catch (\Exception $e) {
             http_response_code(500);
