3850787029ff56d265d296f20d7c32dd2abf15fb
Luiz Fellipe <luizfellipebr03@gmail.com>
2023-04-18 09:34:48 -0300

    API Class implemented

---
+++ App/API.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+class API{
+    private $service;
+    private $method;
+    private $url;
+
+    public function __construct($url)
+    {
+        $this->url = explode('/', $url);
+        if($this->url[0] === 'api'){
+            array_shift($this->url);
+            $this->service = 'App\Services\\' . ucfirst($this->url[0] . 'Service');
+            array_shift($this->url);
+            $this->method = strtolower($_SERVER['REQUEST_METHOD']);
+        }
+    }
+
+    public function execute(){
+        if(!$this->service || !$this->method){
+            http_response_code(404);
+            echo json_encode(array('status' => 'error', 'data' => 'Endpoint not found'), JSON_UNESCAPED_UNICODE);
+            exit;
+        }
+        try {
+            $response = call_user_func_array(array(new $this->service, $this->method), $this->url);
+            http_response_code(200);
+            echo json_encode(array('status' => 'success', 'data' => $response));
+            exit;
+        } catch (\Exception $e) {
+            http_response_code(404);
+            echo json_encode(array('status' => 'error', 'data' => $e->getMessage()), JSON_UNESCAPED_UNICODE);
+            exit;
+        }
+    }
+}
